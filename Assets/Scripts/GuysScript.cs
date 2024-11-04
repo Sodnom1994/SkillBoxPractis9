cd8def79b6881ed65090de300af095436449d5d8
Sodnom1994 <93848263+Sodnom1994@users.noreply.github.com>
2024-11-04 10:34:25 +0300

    2

---
+++ Assets/Scripts/GuysScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuysScript : MonoBehaviour
+{
+    public GameObject[] goodGuys;
+    public GameObject[] badGuys;
+    void Start()
+    {
+        
+    }
+}
