4610976e20841afe853e231c032811c8accf875a
Sodnom1994 <93848263+Sodnom1994@users.noreply.github.com>
2024-11-04 13:08:20 +0300

    3

--- Assets/Scripts/GuysScript.cs
+++ Assets/Scripts/GuysScript.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class GuysScript : MonoBehaviour
 {
+
     public GameObject[] goodGuys;
     public GameObject[] badGuys;
+
     void Start()
     {
-        
+        for (int i = 0; i < goodGuys.Length; i++)
+        {
+            badGuys[i].transform.LookAt(goodGuys[i].transform);
+        }
+        for (int i = 0; i < badGuys.Length; i++)
+        {
+            goodGuys[i].transform.LookAt(badGuys[i].transform);
+
+        }
     }
 }
--- Assets/Scripts/SuperManScript.cs
+++ Assets/Scripts/SuperManScript.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class SuperManScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject _superMan;
+    public GameObject[] badGuys;
+    private Rigidbody[] _badGuysRigidBody;
 
-    // Update is called once per frame
-    void Update()
+    private float _tick = 3f;
+    private float _distance;
+    private float _force;
+    [SerializeField] private float _speed;
+
+    private ForceMode _forceMode; 
+
+    private void Start()
     {
-        
+        _superMan = GetComponent<GameObject>();
+        for (int i = 0; i < badGuys.Length; i++)
+        {
+            badGuys[i] = GetComponent<GameObject>();
+            _badGuysRigidBody[i] = badGuys[i].GetComponent<Rigidbody>();
+            _distance = Vector3.Distance(_superMan.transform.position, badGuys[i].transform.position);
+            _superMan.transform.position = Vector3.MoveTowards(_superMan.transform.position, badGuys[i].transform.position, _speed);
+            if(_distance <=0.2f)
+            {
+                //_badGuysRigidBody[i].AddForce()
+            }
+        }
     }
+    //void Update()
+    //{
+    //    _tick -= Time.deltaTime;
+    //    if (_tick <= 0)
+    //    {
+
+
+
+    //        _tick = 3f;
+    //    }
+
+    //}
 }
